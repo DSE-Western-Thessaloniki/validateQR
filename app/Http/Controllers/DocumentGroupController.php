c682c00bd9b921fd325a7271ea63fe6df450dc94
Theofilos Intzoglou <int.teo@gmail.com>
2023-04-10 13:33:20 +0000

    Προσθήκη μοντέλων Document, DocumentGroup

---
+++ app/Http/Controllers/DocumentGroupController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreDocumentGroupRequest;
+use App\Http\Requests\UpdateDocumentGroupRequest;
+use App\Models\DocumentGroup;
+
+class DocumentGroupController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreDocumentGroupRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(DocumentGroup $documentGroup)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(DocumentGroup $documentGroup)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateDocumentGroupRequest $request, DocumentGroup $documentGroup)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(DocumentGroup $documentGroup)
+    {
+        //
+    }
+}
