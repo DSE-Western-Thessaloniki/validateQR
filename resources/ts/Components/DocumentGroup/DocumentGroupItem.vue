f358562da545c9c1d8390fd23d969ae69b12b8f4
Theofilos Intzoglou <int.teo@gmail.com>
2023-04-15 20:22:01 +0000

    Υλοποίηση component για την εμφάνιση ενός γκρουπ εγγράφων

--- resources/ts/Components/DocumentGroup/DocumentGroupItem.vue
+++ resources/ts/Components/DocumentGroup/DocumentGroupItem.vue
@@ -1,9 +1,44 @@
 <script setup lang="ts">
 import { defineProps } from "vue";
+import DocumentGroupStatus from "@/Components/DocumentGroup/DocumentGroupStatus.vue";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faPencil, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(faPencil, faTrash);
 
 const props = defineProps<{
     group: DocumentGroup;
+    index: number;
+    step: number;
 }>();
 </script>
 
-<template></template>
+<template>
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 my-2">
+        <div
+            class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-6 flex flex-row"
+        >
+            <div class="py-2">{{ index + 1 }}. {{ group.name }}</div>
+            <div class="text-gray-500 px-1 grow py-2">
+                ({{ group.documents_count }} έγγραφα)
+            </div>
+            <DocumentGroupStatus
+                :step="step"
+                totalSteps="5"
+                :colors="['red', 'orange', 'green']"
+                class="py-2 px-5"
+            ></DocumentGroupStatus>
+            <div
+                class="bg-amber-400 px-3 py-2 mx-2 rounded-md shadow-sm hover:shadow-xl hover:bg-amber-300 hover:-translate-y-0.5"
+            >
+                <font-awesome-icon :icon="faPencil" size="1x" />
+            </div>
+            <div
+                class="bg-red-500 px-3 py-2 mx-2 rounded-md shadow-sm hover:shadow-xl hover:bg-red-400 hover:-translate-y-0.5"
+            >
+                <font-awesome-icon :icon="faTrash" size="1x" />
+            </div>
+        </div>
+    </div>
+</template>
