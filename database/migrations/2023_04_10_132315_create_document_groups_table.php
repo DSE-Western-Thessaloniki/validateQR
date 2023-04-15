c6de892bd1c296d27f4fa9e8431f2996ed76e84d
Theofilos Intzoglou <int.teo@gmail.com>
2023-04-15 20:18:11 +0000

    Διόρθωση πεδίου user_id και προσθήκη πεδίου για το τρέχον βήμα στο οποίο βρίσκεται το γκρουπ

--- database/migrations/2023_04_10_132315_create_document_groups_table.php
+++ database/migrations/2023_04_10_132315_create_document_groups_table.php
@@ -13,8 +13,9 @@ return new class extends Migration
     {
         Schema::create('document_groups', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_id');
+            $table->ulid('user_id');
             $table->string('name');
+            $table->unsignedInteger('step');
             $table->timestamps();
         });
     }
