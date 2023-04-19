bc60ed54abbd2de24f66b100812cf8e7a7887dcc
Theofilos Intzoglou <int.teo@gmail.com>
2023-04-19 18:46:58 +0000

    Προσθήκη εφέ για την εναλλαγή των σελίδων

--- resources/ts/Layouts/AppLayout.vue
+++ resources/ts/Layouts/AppLayout.vue
@@ -508,9 +508,23 @@ const logout = () => {
             </header>
 
             <!-- Page Content -->
-            <main>
-                <slot />
-            </main>
+            <Transition name="fade" mode="out-in" appear>
+                <main :key="$page.url">
+                    <slot />
+                </main>
+            </Transition>
         </div>
     </div>
 </template>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+    opacity: 0;
+}
+</style>
