f7adb9f45a06e3949dc16327617e12e92e78c6d6
Theofilos Intzoglou <int.teo@gmail.com>
2023-04-23 21:50:18 +0000

    Διόρθωση τύπου μοντέλου

--- resources/ts/models.d.ts
+++ resources/ts/models.d.ts
@@ -1,8 +1,8 @@
-type DocumentGroup = {
+export type DocumentGroup = {
     id: number;
     name: String;
     step: number;
     published: boolean;
-    created_at: Date;
-    updated_at: Date;
+    created_at: string;
+    updated_at: string;
 };
