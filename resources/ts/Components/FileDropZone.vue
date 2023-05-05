11499164f27c283c9445e9b3f150b5549e2c6873
Theofilos Intzoglou <int.teo@gmail.com>
2023-05-05 20:19:22 +0000

    Χρήση cypress για testing Fixes #1

---
+++ resources/ts/Components/FileDropZone.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div
+        class="cursor-pointer m-3 p-2 w-auto h-64 bg-indigo-500 flex flex-col items-center justify-center rounded border-2 border-indigo-700 shadow shadow-2xl"
+    >
+        <span class="text-white text-center">
+            Σύρετε τα αρχεία εδώ μέσα ή πατήστε για να εμφανιστεί το παράθυρο
+            διαλόγου</span
+        >
+    </div>
+</template>
