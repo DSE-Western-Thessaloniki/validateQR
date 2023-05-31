fb01635208cc8e045f61d8222441b7ba037e704f
Theofilos Intzoglou <int.teo@gmail.com>
2023-05-31 19:16:48 +0000

    Εμφάνιση αρχείων και σημείωση στην περίπτωση που έχει ήδη γίνει επεξεργασία τους

--- resources/ts/Components/DocumentGroup/Wizard/Step3.vue
+++ resources/ts/Components/DocumentGroup/Wizard/Step3.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import { useWizardStore } from "@/Stores/wizard";
+import { faCircleCheck } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 const wizard = useWizardStore();
 
@@ -49,7 +51,34 @@ const save = () => {
 defineExpose({ save });
 </script>
 <template>
-    <div class="flex flex-row bg-white p-4 m-4 rounded items-center">
+    <div class="flex flex-col bg-white p-4 m-4 rounded items-center">
+        <div class="font-bold">Ανεβασμένα αρχεία</div>
+        <div
+            class="flex flex-row h-72 w-full bg-gray-100 rounded shadow-inner items-center justify-center m-3"
+        >
+            <div class="" v-if="!wizard.documents?.length">
+                Δεν έχουν ανέβει αρχεία ακόμη!
+            </div>
+            <div v-else class="flex flex-col h-72 w-full overflow-y-auto">
+                <div
+                    v-for="(document, index) in wizard.documents"
+                    class="bg-white rounded m-2 p-2 shadow flex flex-row items-center"
+                >
+                    <div class="flex-grow text-ellipsis">
+                        {{ index + 1 }}. {{ document.filename }}
+                    </div>
+                    <span class="bg-orange-300 p-2 ml-2 rounded shadow">{{
+                        document.id
+                    }}</span>
+                    <FontAwesomeIcon
+                        :icon="faCircleCheck"
+                        v-if="document.state > 0"
+                        class="px-2 text-green-600"
+                        size="2x"
+                    />
+                </div>
+            </div>
+        </div>
         <div>Λήψη αρχείων με QR Code</div>
     </div>
 </template>
