669cffe170b793e8a8636523520cedc5142ba054
Theofilos Intzoglou <int.teo@gmail.com>
2023-09-23 16:58:07 +0000

    Προσθήκη δυνατότητας για δημιουργία zip και για τα υπογεγραμμένα έγγραφα

--- app/Jobs/ZipDocumentGroup.php
+++ app/Jobs/ZipDocumentGroup.php
@@ -2,6 +2,7 @@
 
 namespace App\Jobs;
 
+use App\Models\Document;
 use App\Models\DocumentGroup;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldBeUnique;
@@ -15,7 +16,13 @@ class ZipDocumentGroup implements ShouldQueue, ShouldBeUnique
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    public $documentGroup;
+    public DocumentGroup $documentGroup;
+
+    /** @var int $documentType
+     * Ο τύπος των αρχείων που θα συμπιέσουμε. Αποδεκτές τιμές είναι οι
+     * Document::WithQR και Document::WithQRAndSignature
+     */
+    public int $documentType;
 
     public $backoff = 2;
 
@@ -24,9 +31,10 @@ class ZipDocumentGroup implements ShouldQueue, ShouldBeUnique
     /**
      * Create a new job instance.
      */
-    public function __construct(DocumentGroup $documentGroup)
+    public function __construct(DocumentGroup $documentGroup, int $type = Document::WithQR)
     {
         $this->documentGroup = $documentGroup;
+        $this->documentType = $type;
     }
 
     /**
@@ -34,11 +42,22 @@ class ZipDocumentGroup implements ShouldQueue, ShouldBeUnique
      */
     public function handle(): void
     {
+        // Σταμάτησε εγκαίρως αν δεν αναγνωρίζουμε τον τύπο των εγγράφων
+        if (!in_array($this->documentType, [Document::WithQR, Document::WithQRAndSignature])) {
+            $this->documentGroup->job_status = DocumentGroup::JobFailed;
+            $this->documentGroup->job_status_text = 'Άκυρος τύπος εγγράφων για συμπίεση';
+            $this->documentGroup->save();
+            $this->fail('Άκυρος τύπος εγγράφων για συμπίεση');
+            return;
+        }
+
+        $documentDir = Document::WithQR ? 'qr' : 'signed';
+
         $this->documentGroup->job_status_text = 'Συμπίεση αρχείων';
         $this->documentGroup->save();
 
         $zip = new ZipArchive();
-        $filename = storage_path('app')."/{$this->documentGroup->id}/qr/{$this->documentGroup->id}.zip";
+        $filename = storage_path('app')."/{$this->documentGroup->id}/{$documentDir}/{$this->documentGroup->id}.zip";
 
         if (! $zip->open($filename, ZipArchive::CREATE | ZipArchive::OVERWRITE)) {
             $message = "Αποτυχία συμπίεσης! {$zip->getStatusString()}";
@@ -51,9 +70,9 @@ class ZipDocumentGroup implements ShouldQueue, ShouldBeUnique
         }
 
         foreach ($this->documentGroup->documents()->get() as $document) {
-            $document_filename = storage_path('app')."/{$this->documentGroup->id}/qr/{$document->id}.pdf";
+            $document_filename = storage_path('app')."/{$this->documentGroup->id}/{$documentDir}/{$document->id}.pdf";
             logger('Zip: Προσθήκη αρχείου: '. $document_filename);
-            if (! $zip->addFile($document_filename, "{$document->id}.pdf")) {
+            if (! $zip->addFile($document_filename, "{$document->filename}")) {
                 $message = "Αποτυχία συμπίεσης αρχείου {$document->id}.pdf! {$zip->getStatusString()}";
                 $this->documentGroup->job_status = DocumentGroup::JobFailed;
                 $this->documentGroup->job_status_text = $message;
